660758079c8037d37674f6e1f8415e5ecdc9fcb2
Evan He <eh4359@rit.edu>
2024-12-24 23:17:40 -0500

    prop types validation

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,20 +1,31 @@
 import {
     Panel,
 } from '@xyflow/react';
+import PropTypes from 'prop-types';
 
 
 function CustomPanel({ position, createNode, toggleModes }) {
     return (
         <Panel className="custom-panel" position={position}>
             <div id="toolbar">
-                <button id="add-node-btn" onClick={createNode}>Add Node</button>
+                <div>
+                    <button id="add-node-btn" onClick={createNode}>Add Node</button>
+                </div>
                 <div>
                     <button id="connect-mode" onClick={() => toggleModes('connect')}>Connect Mode</button>
+                </div>
+                <div>
                     <button id="move-mode" onClick={() => toggleModes('move')}>Move Mode</button>
                 </div>
             </div>
         </Panel>
     );
 }
+
+CustomPanel.propTypes = {
+    position: PropTypes.string.isRequired,
+    createNode: PropTypes.func.isRequired,
+    toggleModes: PropTypes.func.isRequired,
+};
 
 export default CustomPanel;
