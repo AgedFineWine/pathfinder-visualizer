0475999e0241d1a1d243c877bafe65d505b829be
Evan He <eh4359@rit.edu>
2024-12-26 15:10:30 -0500

    customized panel bar

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -3,6 +3,8 @@ import {
     type PanelPosition,
 } from '@xyflow/react';
 
+import './Panel.css';
+
 // PanelProps is supposed to have chlidren. So this custom type is not an extension of PanelProps.
 // This is just a wrapper.
 type CustomPanelProps = {
@@ -15,14 +17,14 @@ function CustomPanel({ position, createNode, toggleModes }: CustomPanelProps) {
     return (
         <Panel className="custom-panel" position={position}>
             <div id="toolbar">
-                <div>
+                <div className="btn-container">
                     <button id="add-node-btn" onClick={createNode}>Add Node</button>
                 </div>
-                <div>
-                    <button id="connect-mode" onClick={() => toggleModes('connect')}>Connect Mode</button>
+                <div className="btn-container">
+                    <button id="connect-mode" onClick={() => toggleModes('connect')}>Connect Nodes</button>
                 </div>
-                <div>
-                    <button id="move-mode" onClick={() => toggleModes('move')}>Move Mode</button>
+                <div className="btn-container">
+                    <button id="move-mode" onClick={() => toggleModes('move')}>Move Nodes</button>
                 </div>
             </div>
         </Panel>
