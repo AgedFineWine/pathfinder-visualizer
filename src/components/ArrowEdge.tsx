97175c33aae83853e2fe4080f1cf55cff7873673
Evan He <eh4359@rit.edu>
2024-12-25 19:03:06 -0500

    base commit for customized arrow edge

---
+++ src/components/ArrowEdge.tsx
@@ -0,0 +1,34 @@
+import {
+    getStraightPath,
+    useInternalNode,
+    EdgeProps,
+} from '@xyflow/react';
+
+import { getEdgeParams } from '../utils/utils.ts';
+
+function ArrowEdge({ id, source, target, markerEnd, style }: EdgeProps) {
+    const sourceNode = useInternalNode(source);
+    const targetNode = useInternalNode(target);
+
+    if (!sourceNode || !targetNode) return null;
+
+    const { sx, sy, tx, ty } = getEdgeParams(sourceNode, targetNode);
+
+    const [edgePath] = getStraightPath({
+        sourceX: sx,
+        sourceY: sy,
+        targetX: tx,
+        targetY: ty,
+    });
+
+    return (
+        <path 
+            id={id}
+            className="react-flow__edge-path"
+            d={edgePath}
+            markerEnd={markerEnd}
+        />
+    );
+}
+
+export default ArrowEdge;
