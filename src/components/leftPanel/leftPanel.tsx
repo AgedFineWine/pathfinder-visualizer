6a3fcdfd9a3ef0fab3bdd39550c9dac80a5a6282
Evan He <eh4359@rit.edu>
2024-12-30 17:26:58 -0500

    included options to create start and destination nodes

--- src/components/leftPanel/leftPanel.tsx
+++ src/components/leftPanel/leftPanel.tsx
@@ -4,29 +4,31 @@ import {
     type PanelPosition,
 } from '@xyflow/react';
 
-// import connectionBtn from '../../assets/connection-btn.svg';
+import { Mode } from '../../utils/enums';
+
 import styles from './leftPanel.module.css';
 
 import connectionBtn from '../../assets/connection.svg';
 import add from '../../assets/add.svg';
 import arrows from '../../assets/arrows.svg';
+import arrowRight from '../../assets/arrow-right.svg';
+import goal from '../../assets/goal.svg';
 
 // PanelProps is supposed to have chlidren. So this custom type is not an extension of PanelProps.
 // This is just a wrapper.
 type LeftPanelProps = {
     position: PanelPosition,
     defaultMode: string,
-    createNode: () => void,
-    toggleModes: (newMode: string) => void,
+    toggleModes: (newMode: Mode) => void,
 };
 
-function LeftPanel({ position, createNode, toggleModes, defaultMode }: LeftPanelProps) {
-    const [active, setActive] = useState('unweighted');
+function LeftPanel({ position, toggleModes, defaultMode }: LeftPanelProps) {
+    const [active, setActiveGraph] = useState('unweighted');
 
     const [activeMode, setActiveMode] = useState(defaultMode);
 
-    const handleActive = (btnName: string) => {
-        setActive(btnName);
+    const handleActiveGraph = (btnName: string) => {
+        setActiveGraph(btnName);
     };
 
     const handleActiveMode = (mode: string) => {
@@ -53,7 +55,7 @@ function LeftPanel({ position, createNode, toggleModes, defaultMode }: LeftPanel
                             ${styles['weight-btn']}
                             ${active === 'weighted' ? `${styles['active-color']}` : ''}
                             `}
-                            onClick={() => handleActive('weighted')}
+                            onClick={() => handleActiveGraph('weighted')}
                         >
                             Weighted Graph
                         </button>
@@ -64,7 +66,7 @@ function LeftPanel({ position, createNode, toggleModes, defaultMode }: LeftPanel
                             ${styles['weight-btn']}
                             ${active === 'unweighted' ? `${styles['active-color']}` : ''}
                             `}
-                            onClick={() => handleActive('unweighted')}
+                            onClick={() => handleActiveGraph('unweighted')}
                         >
                             Unweighted Graph
                         </button>
@@ -77,11 +79,11 @@ function LeftPanel({ position, createNode, toggleModes, defaultMode }: LeftPanel
                         <div>
                             <button className={`
                                 ${styles['svg-btn']}
-                                ${activeMode === 'move' ? `${styles['active-color']}` : ''}
+                                ${activeMode === Mode.Move ? `${styles['active-color']}` : ''}
                                 `}
                                 onClick={() => {
-                                    toggleModes('move');
-                                    handleActiveMode('move');
+                                    toggleModes(Mode.Move);
+                                    handleActiveMode(Mode.Move);
                                 }}
                                 title="Move Nodes"
                             >
@@ -92,36 +94,54 @@ function LeftPanel({ position, createNode, toggleModes, defaultMode }: LeftPanel
                             <button
                                 className={`
                                     ${styles['svg-btn']}
-                                    ${activeMode === 'connect' ? `${styles['active-color']}` : ''}
+                                    ${activeMode === Mode.Connect ? `${styles['active-color']}` : ''}
                                 `}
                                 onClick={() => {
-                                    toggleModes('connect');
-                                    handleActiveMode('connect');
+                                    toggleModes(Mode.Connect);
+                                    handleActiveMode(Mode.Connect);
                                 }}
                                 title="Connect Nodes"
                             >
                                 <img src={connectionBtn} loading="lazy" alt="Connect Nodes" className={`${styles['svg-img']}`} />
                             </button>
                         </div>
+                        <div>
+                            <button className={`
+                                ${styles['svg-btn']}
+                                ${activeMode === Mode.Add ? `${styles['active-color']}` : ''}
+                            `} onClick={() => {
+                                    toggleModes(Mode.Add);
+                                    handleActiveMode(Mode.Add);
+                                }} title="Add Node">
+                                <img src={add} loading="lazy" alt="Plus sign" className={`${styles['svg-img']}`} />
+                            </button>
+                        </div>
                     </div>
                 </section>
 
                 <section>
-                    <span>Insertions</span>
+                    <span>Node Selectors</span>
                     <div className={`${styles['insertion-selection']}`}>
                         <div>
-                            <button className={`${styles['svg-btn']}`} onClick={createNode} title="Add Node">
-                                <img src={add} loading="lazy" alt="Plus sign" className={`${styles['svg-img']}`} />
+                            <button className={`
+                                ${styles['svg-btn']}
+                                ${activeMode === Mode.StartSelect ? `${styles['active-color']}` : ''}
+                            `} onClick={() => {
+                                    toggleModes(Mode.StartSelect);
+                                    handleActiveMode(Mode.StartSelect);
+                                }} title="Start Node">
+                                <img src={arrowRight} loading="lazy" alt="Arrow right" className={`${styles['svg-img']}`} />
                             </button>
                         </div>
                         <div>
-                            <button className={`${styles['svg-btn']}`} onClick={createNode} title="Add Node">
-                                <img src={add} loading="lazy" alt="Plus sign" className={`${styles['svg-img']}`} />
-                            </button>
-                        </div>
-                        <div>
-                            <button className={`${styles['svg-btn']}`} onClick={createNode} title="Add Node">
-                                <img src={add} loading="lazy" alt="Plus sign" className={`${styles['svg-img']}`} />
+                            <button className={`
+                                ${styles['svg-btn']}
+                                ${activeMode === Mode.DestinationSelect ? `${styles['active-color']}` : ''}
+                            `} onClick={() => {
+                                    toggleModes(Mode.DestinationSelect);
+                                    handleActiveMode(Mode.DestinationSelect);
+                                }} title="Destination Node">
+                                <img src={goal} loading="lazy" alt="Goal icon" className={`${styles['svg-img']}`} />
                             </button>
                         </div>
                     </div>
