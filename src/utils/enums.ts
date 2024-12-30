69fef0de2e805b1046b548f6931e360acaded051
Evan He <eh4359@rit.edu>
2024-12-30 17:27:39 -0500

    enums for different possible modes and the terminal type in a node

---
+++ src/utils/enums.ts
@@ -0,0 +1,12 @@
+export enum Mode {
+	Connect = 'connect',
+	Move = 'move',
+	Add = 'add',
+	StartSelect = 'start-select',
+	DestinationSelect = 'destination-select',
+}
+
+export enum Terminal {
+    Start = 'start',
+    Destination = 'destination',
+}
