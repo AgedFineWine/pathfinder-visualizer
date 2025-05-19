f6fc454a57e399533ece6e79411498460451fa9b
Evan He <eh4359@rit.edu>
2025-05-18 21:31:04 -0400

    backend init commit

---
+++ pathfinder-core/src/main/java/com/pathfinder/pathfindercore/PathfinderCoreApplication.java
@@ -0,0 +1,13 @@
+package com.pathfinder.pathfindercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderCoreApplication.class, args);
+	}
+
+}
---
+++ pathfinder-core/src/test/java/com/pathfinder/pathfindercore/PathfinderCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathfinder.pathfindercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
